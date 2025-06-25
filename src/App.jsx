929784e1388e628779151ae62fbabf114b5a8c46
Cesar Preza <cpreza93@yahoo.com>
2025-06-25 14:39:44 -0700

    adds navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <h1>Welcome to Raydex</h1>
+      <p>An app built for Radiologic Professionals and Students</p>
     </>
   )
 }
